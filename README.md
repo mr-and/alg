69a6a0b8a77444ea451a254e2da62ba212d4eea6
av.bobrov <av.bobrov@bftcom.com>
2022-07-11 16:53:59 +0300

    Java #2

--- README.md
+++ README.md
@@ -6,4 +6,5 @@ LeetCode
 
 | # | Title | Solution                                                                               | Difficulty |
 |---| ----- |----------------------------------------------------------------------------------------| ---------- |
+|2|[Palindrome Number](https://leetcode.com/problems/palindrome-number)| [Java](./java/src/palindromeNumber/PalindromeNumber.java)|Easy|
 |1|[Two Sum](https://leetcode.com/problems/two-sum/)| [Java](./java/src/twoSum/TwoSum.java)|Easy|
---
+++ java/src/palindromeNumber/PalindromeNumber.java
@@ -0,0 +1,23 @@
+/**********************************************************************************
+ *
+ * Given an integer x, return true if x is palindrome integer.
+ *
+ * An integer is a palindrome when it reads the same backward as forward.
+ *
+ *
+ **********************************************************************************/
+
+package palindromeNumber;
+
+public class PalindromeNumber {
+
+    public boolean isPalindrome(int x) {
+        String str = String.valueOf(x);
+        int start = 0;
+        int end = str.length() - 1;
+        while (start < end) {
+            if (str.charAt(start++) != str.charAt(end--)) return false;
+        }
+        return true;
+    }
+}
