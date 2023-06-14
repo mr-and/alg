5d8af7ebc3164f74312a1664779194b9a5d095ff
av.bobrov <av.bobrov@bftcom.com>
2023-06-14 16:16:02 +0300

    Java #10

---
+++ java/src/problem10/ReverseLinkedList.java
@@ -0,0 +1,39 @@
+/**********************************************************************************
+ *
+ * Given the head of a singly linked list, reverse the list, and return the reversed list.
+ *
+ *
+ **********************************************************************************/
+
+package problem10;
+
+public class ReverseLinkedList {
+    public static ListNode reverseList(ListNode head) {
+        ListNode newHead = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = next;
+        }
+        return newHead;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+}
